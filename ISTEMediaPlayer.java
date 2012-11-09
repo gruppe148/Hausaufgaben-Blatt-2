91a37f9565ba4d05c948928e24fc54645333ffd2
lemen <les_lemen@ymail.com>
2012-11-09 13:16:53 +0100

    ASCII art added and fuction calls are different

--- ISTEMediaPlayer.java
+++ ISTEMediaPlayer.java
@@ -1,7 +1,7 @@
 /*
 *****************************************************************
 *	    Name: Peter Faber (2836641), Clemens Mayer (2815170), Han Ngoc Phan (2821355)
-*	   Datum: Tue Oct 30 18:33:09 CET 2012			
+*	   Datum: Fri Nov  9 13:13:48 CET 2012 
 *	 Aufgabe: IsteMediaPlayer				
 *	 Version: 1.0						
 *	Funktion: Funktionsaufrufe von Klassen in iste.media.*
@@ -16,18 +16,47 @@ class ISTEMediaPlayer implements ISTEMediaInterface
    {
 
 	/* Aufruf aller Methoden von MaschenDrahtZaunSong */
+	/* Aufruf der Getter*/
 	MaschenDrahtZaunSong.getDescription();
 	MaschenDrahtZaunSong.getDuration();
-	MaschenDrahtZaunSong.play();
+	/*Spielt 2 Sekunden das Lied da man es länger nicht aushält :) */
+	MaschenDrahtZaunSong.play(2);
 
-	/* Aufruf aller Methoden von TourSong */
+	/* Aufruf aller Methoden von TourSong Analog zum ersten */
 	TourSong.getDescription();
 	TourSong.getDuration();
-	TourSong.play();
+	TourSong.play(45);
+
+
+/* **************************************************
+*	         	 /|         ,
+*	               ,///        /|
+*	              // //     ,///
+*	             // //     // //
+*	            // //     || ||
+*	            || ||    // //
+*	            || ||   // //
+*	            || ||  // //
+*	            || || || ||
+*	            \\,\|,|\_//
+*	             \\)\)\\|/
+*	             )-."" .-(
+*	            //^\` `/^\\
+*	           //  |   |  \\  
+*	         ,/_| 0| _ | 0|_\,
+*	       /`    `"=.v.="`    `\
+*	      /`    _."{_,_}"._    `\
+*	 jgs  `/`  ` \  |||  / `  `\`
+*	       `",_  \\=^~^=//  _,"`
+*	           "=,\'-=-'/,="
+*	               '---'
+*
+* 	ASCII ART by http://www.geocities.com/spunk1111/warnerb.htm#bugs
+************************************************************/
  
 	/* Aufruf aller Methoden von BugsBunnyMusik */
 	BugsBunnyMusik.getDescription();
 	BugsBunnyMusik.getDuration();
-	BugsBunnyMusik.play();
+	BugsBunnyMusik.play(35);
    }
 }
